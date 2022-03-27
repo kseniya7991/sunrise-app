1b0dab2bd12a00899507f1d1d738c3e7be03038f
Kseniya St <kseniya7991@gmail.com>
2022-03-27 22:22:12 +0300

    process data from API (sunrise time, time to next sunrise)

--- sunrise-app/src/App.js
+++ sunrise-app/src/App.js
@@ -1,15 +1,53 @@
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 /* import { getWeather } from "./utils/api"; */
 import respJSON from "./resp.json";
 
 function App() {
+  const [timeNextSun, setTimeNextSun] = useState("");
+  const [timeToNextSun, setTimeToNextSun] = useState("");
+  const [test, setTest] = useState(0);
+  let currentDate = new Date();
+
   function getData() {
-    console.log(respJSON);
+    const unixTimeNextSun = respJSON.daily[1].sunrise;
+
+    if (unixTimeNextSun) {
+      let date = new Date(unixTimeNextSun * 1000);
+      let h = date.getHours();
+      let m = date.getMinutes();
+      setTimeNextSun(`${h}:${m}`);
+      let nextSunH = Math.floor(
+        (date.valueOf() - currentDate.valueOf()) / 1000 / 60 / 60
+      ).toString();
+      let nextSunM = Math.floor(
+        ((date.valueOf() - currentDate.valueOf()) / 1000 / 60) % 60
+      );
+      let nextSunS = Math.floor(
+        ((date.valueOf() - currentDate.valueOf()) / 1000) % 60
+      );
+      /*   setTimeToNextSun(
+        `${nextSunH.length === 1 ? "0" + nextSunH : nextSunH}:${
+          nextSunM.length === 1 ? "0" + nextSunM : nextSunM
+        }`
+      ); */
+      setTimeToNextSun(`${nextSunH}, ${nextSunM}`);
+    }
+
     /* getWeather()
       .then((res) => console.log(res, respJSON))
       .catch((err) => console.log(err)); */
   }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getData();
+      setTest(timeToNextSun);
+      console.log(timeToNextSun);
+    }, 60000);
+    return () => clearInterval(interval);
+  }, [test]);
+  console.log(test);
 
   return (
     <div className="App">
@@ -21,7 +59,16 @@ function App() {
         <button className="button" onClick={getData}>
           Нажми сюда
         </button>
-        <div className="table">Learn React</div>
+        <ul className="table">
+          <li>
+            <span>Время следующего рассвета</span>
+            <span>{timeNextSun}</span>
+          </li>
+          <li>
+            <span>Время до следующего рассвета</span>
+            <span>{test}</span>
+          </li>
+        </ul>
       </header>
     </div>
   );
